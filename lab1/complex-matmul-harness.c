322e54106f4febceeb22058ae3739688ad5141e6
Daniel Connaughton <connaud@cs.tcd.ie>
2016-02-28 21:50:06 +0000

    Added omp parallel for loops

--- lab1/complex-matmul-harness.c
+++ lab1/complex-matmul-harness.c
@@ -153,8 +153,27 @@ void matmul(struct complex ** A, struct complex ** B, struct complex ** C, int a
 
 /* the fast version of matmul written by the team */
 void team_matmul(struct complex ** A, struct complex ** B, struct complex ** C, int a_rows, int a_cols, int b_cols) {
-  //replace this
-  matmul(A, B, C, a_rows, a_cols, b_cols);
+ int i, j, k;
+
+ #pragma omp parallel for
+ for(i=0; i<a_rows; i++){
+   #pragma omp parallel for
+   for(j=0; j<b_cols; j++){
+     struct complex sum;
+     sum.real = 0.0;
+     sum.imag = 0.0;
+     #pragma omp parallel for
+     for(k=0; k<a_cols; k++){
+       // the following code does: sum += A[i][k] * B[k][j];
+       struct complex product;
+       product.real = A[i][k].real * B[k][j].real - A[i][k].imag * B[k][j].imag;
+       product.imag = A[i][k].real * B[k][j].imag + A[i][k].imag * B[k][j].real;
+       sum.real += product.real;
+       sum.imag += product.imag;
+   }
+   C[i][j] = sum;
+  }
+ }
 }
 
 long long time_diff(struct timeval * start, struct timeval * end) {
